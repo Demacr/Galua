ab8605b206df8744352a06338c74e0d86a70e220
Karandashov Daniil <karandashovd@gmail.com>
2012-12-14 00:54:23 +0600

    Add irred_polynoms(power) function which calculate irreduced polynomials to power power :)

--- galua.py
+++ galua.py
@@ -9,7 +9,10 @@ class galua:
 		print(other.factors)
 		for i in range(1, len(temp)):
 			temp[-i] = (temp[-i] + other.factors[-i]) % 2
-		return galua(temp[temp.index(1):])
+		if temp.count(1) > 0:
+			return galua(temp[temp.index(1):])
+		else:
+			return galua([0])
 		
 	def __mul__(self, other):
 		temp = [0] * (max(len(self.factors), len(other.factors)) * 2)
@@ -18,7 +21,10 @@ class galua:
 				mul = [0] * (len(temp) - i + 1 - len(self.factors)) + self.factors + [0] * (i - 1)
 				for j in range(1, len(temp)):
 					temp[-j] = (temp[-j] + mul[-j]) % 2
-		return galua(temp[temp.index(1):])
+		if temp.count(1) > 0:
+			return galua(temp[temp.index(1):])
+		else:
+			return galua([0])
 		
 	def __mod__(self, other):
 		temp = self.factors[:]
@@ -27,7 +33,10 @@ class galua:
 			mod = other.factors + [0] * (len(temp) - temp.index(1) - length)
 			for i in range(1, len(mod) + 1):
 				temp[-i] = (temp[-i] + mod[-i]) % 2
-		return galua(temp)
+		if temp.count(1) > 0:
+			return galua(temp[temp.index(1):])
+		else:
+			return galua([0])
 		
 	def __str__(self):
 		return self.info(self.factors)
@@ -37,10 +46,7 @@ class galua:
 				
 	def info(self, value):
 		str = ''.join(map(lambda x: chr(x + 48), value))
-		if str.count('1') > 0:
-			return str[str.index('1'):]
-		else:
-			return str
+		return str
 
 	
 def bs(s):
@@ -52,3 +58,25 @@ def muls(pln, mod):
 	for i in range(1, 256):
 		gal = galua(bs(i))
 		print('temp =', gal, (gal * const % module))
+		
+def irred_polynoms(power):
+	if not isinstance(power, int):
+		raise ValueError("power is not int")
+	if power < 1:
+		raise ValueError("power is less than 1")
+	print("irreduced polynomial")
+	for i in range(1, 2 ** power):
+		#print("i =", i)
+		count = 0
+		module = galua(bs(i))
+		for j in range(1, i + 1):
+			polynomial = module % galua(bs(j)) 
+			#print("poly =", polynomial)
+			if len(polynomial.factors) == 1 and polynomial.factors[0] == 0:
+				count = count + 1
+				if count > 2:
+					break
+		if count > 2:
+			continue
+		else:
+			print(module)
